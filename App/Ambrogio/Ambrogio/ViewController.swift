5a3c588fe7e99e7d82414f7cc65fd8d3c1eba3ce
Gruppioni Michele <gruppiofigo@hotmail.it>
2016-12-04 16:11:15 +0100

    Completed the basic storyboard

--- App/Ambrogio/Ambrogio/ViewController.swift
+++ App/Ambrogio/Ambrogio/ViewController.swift
@@ -10,16 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var textView: UITextView!
+    
+    let particleURLString = "https://api.particle.io/v1/devices/1a002b000447343337373737/execute"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func didTapTheScreen(_ sender: Any) {
+        print("Screen Tapped")
     }
-
-
+    
+    private func sendRequest() {
+        
+        //let urlRequest = URLRequest(url: NSURL())
+        //URLSession.shared.dataTask(with: <#T##URLRequest#>, completionHandler: <#T##(Data?, URLResponse?, Error?) -> Void#>)
+    }
 }
 
