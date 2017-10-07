ee1c2dfa9d980b80e782c670d795d063a507543b
Gruppioni Michele <gruppiofigo@hotmail.it>
2017-10-07 14:34:15 +0200

    Appliance Names + new command names

---
+++ AppliancesNames.h
@@ -0,0 +1,28 @@
+//
+//  AppliancesNames.h
+//  Ambrogio
+//
+//  Created by Gruppioni Michele on 07/10/2017.
+//  Copyright © 2017 Michele Gruppioni. All rights reserved.
+//
+
+#ifndef AppliancesNames_h
+#define AppliancesNames_h
+
+#define LED_PANEL "LedPanel"
+#define LIVING_ROOM_LAMP "LivingRoomLamp"
+#define LED_STRIP_LEFT "LedStripLeft"
+#define LED_STRIP_RIGHT "LedStripRight"
+#define COFFEE_MACHINE "CoffeeMachine"
+#define TV "Tv"
+#define CHRISTMAS_TREE "ChristmasTree"
+#define FAN "Fan"
+#define KITCHEN_LIGHT "KitchenLight"
+#define LIVINGROOM_LIGHTS "LivingRoomLights"
+#define LIVINGROOM_LIGHT_1 "LivingRoomLight1"
+#define LIVINGROOM_LIGHT_2 "LivingRoomLight2"
+#define LIVINGROOM_LIGHT_3 "LivingRoomLight3"
+#define MOON "Moon"
+#define UNKNOWN_APPLIANCE "UnknownAppliance"
+
+#endif /* AppliancesNames_h */
